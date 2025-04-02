765e8402cd04db6b0a81b486195b755739041f41
lucasxnarezzi <lucasxnarezzi@gmail.com>
2025-04-02 12:28:39 -0300

    alterando nomenclaturas

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-valor = prompt("Olá, quantos dólares você tem na aí?");
-real = valor * 5.70;
+dolar = prompt("Olá, quantos dólares você tem na aí?");
+real = dolar * 5.70;
 alert("Isso equivale a R$" + real + " reais")
+// O código acima converte dólares para reais, considerando a cotação de 5.70 reais por dólar. 
+ 
